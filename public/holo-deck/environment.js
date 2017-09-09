1265695170b17d2f42f4d64bef5fc379fa5ad321
Keith Irwin <keith@zentrope.com>
2017-09-09 10:21:58 -0700

    Remove accidental example app.

--- public/holo-deck/environment.js
+++
@@ -1,3 +0,0 @@
-window.env = {
-  endpoint: "/api"
-}
--- public/holo-deck/metadata.js
+++
@@ -1,8 +0,0 @@
-{
-  "xrn": "proxy:app:ui:26ad303c-baab-42b8-85b9-0bc50c4d1672",
-  "name": "Holodeck",
-  "description": "Schedules, recommendations, reservations.",
-  "version" : "0.23",
-  "date": "2017-08-15",
-  "author": "Stern & Traxis"
-}
